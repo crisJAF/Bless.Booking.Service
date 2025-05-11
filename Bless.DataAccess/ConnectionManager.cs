75c3fca79ad6f29e9f9759f5156dfcbf3ce43e54
douglas-i <hdouglasu@gmail.com>
2025-05-11 13:12:00 -0600

    feat: added ConnectionManager

---
+++ Bless.DataAccess/ConnectionManager.cs
@@ -0,0 +1,23 @@
+﻿using Bless.DataAccess.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Bless.DataAccess
+{
+    public class ConnectionManager : IConnectionManager
+    {
+        public const string connectionStringKey = "BarberDB";
+        private readonly IConfiguration configuration;
+
+        public ConnectionManager(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IDbConnection GetConnectionString(string key)
+        {
+            return new SqlConnection(ConfigurationExtensions.GetConnectionString(configuration, $"{key}"));
+        }
+    }
+}
