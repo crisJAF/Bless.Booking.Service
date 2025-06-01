3d9bfb1e22cfe74c7be44fd2fded76ef6cef811d
douglas-i <hdouglasu@gmail.com>
2025-05-31 23:09:05 -0600

    feat(auth): implement login logic with validation and JWT generation

---
+++ Bless.BusinessLogic.Interfaces/IUsuario.cs
@@ -0,0 +1,15 @@
+﻿using Bless.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bless.BusinessLogic.Interfaces
+{
+    public interface IUsuario
+    {
+        public Task<Response<LoginResponse>> LoginAsync(LoginRequest request);
+        public Task<Response<string>> RegistrarAsync(Usuario usuario);
+    }
+}
---
+++ Bless.Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bless.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string ContrasenaHash { get; set; }
+        public string Rol { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+
+}
